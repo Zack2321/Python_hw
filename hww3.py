0ea3ae25162ae632326f43e6389672c3e72cd3db
Zack2321 <138007403+Zack2321@users.noreply.github.com>
2023-09-23 22:26:34 +0630

    Delete hww3.py

--- hww3.py
+++
@@ -1,9 +0,0 @@
-name= print("name_list = [Mark, Nicole, Jason, Stephanie]")
-name_list = ["Mark ", "Nicole", "Jason", "Stephanie"]
-
-# Display the first and third names
-first_name = name_list[0]
-third_name = name_list[2]
-
-# Print the result
-print(f"Answer: {first_name + third_name}")
