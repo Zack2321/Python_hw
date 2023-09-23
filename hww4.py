fe0da91f816e704f2526e55d9448756546d0c83d
Zack2321 <138007403+Zack2321@users.noreply.github.com>
2023-09-23 22:26:59 +0630

    Delete hww4.py

--- hww4.py
+++
@@ -1,4 +0,0 @@
-fusername = input("Input your name : ")
-age =int(input("What is your age?"))
-yui = age
-print ("Welcome", fusername,"!!!!", "You are ", yui, " years old now. ", "You can acess to this program." )
